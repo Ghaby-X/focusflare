1b07862216ef93386fc0eaa87dc599b20e12b880
Ghaby-X <gabrielanyaele21@gmail.com>
2024-11-16 14:58:55 +0000

    feat: add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,45 @@
+'use client'
+import Link from 'next/link'
 import React from 'react'
+import SettingsModal from './SettingsModal'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
+    const pathName = usePathname()
+
     return (
-        <div>Header</div>
+        <div className='flex p-4'>
+            <div className=''>focusflare</div>
+            <div className='fixed w-screen flex justify-center'>
+                <div className='flex justify-between w-96'>
+                    {
+                        menuItems.map(item => {
+                            let className = ''
+
+                            if (item.link == pathName) {
+                                className = 'text-purple-600 border-b-2 border-purple-900'
+                            }
+                            return <div key={item.title} className={className}>{item.item}</div>
+                        })
+                    }
+                    <SettingsModal />
+                </div>
+            </div>
+        </div>
     )
 }
+
+const menuItems = [
+    {
+        title: 'about',
+        link: '/',
+        item: <Link href={'/'}>about</Link>
+    },
+    {
+        title: 'timer',
+        link: '/timer',
+        item: <Link href={'/timer'}>timer</Link>
+    }
+]
 
 export default Header
---
+++ components/SettingsModal.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const SettingsModal = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="cursor-pointer">settings</div>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Configure settings</DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-row min-h-40 h-full">
+                    <div className="px-2">
+                        hello
+                    </div>
+                    <div className="h-full w-[2px] bg-black"></div>
+                    <div className="px-2">
+                        hello
+                    </div>
+
+                </div>
+                <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default SettingsModal
